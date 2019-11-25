a48184e58f57da296df8b59334820396ad9b6ab9
eric-sonchaiwanich <eric-sonchaiwanich@users.noreply.github.com>
2019-11-24 22:56:16 -0500

    small tweaks

--- server.js
+++ server.js
@@ -15,27 +15,23 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
+  res.render('index', { weather: null, error: null });
 })
 
 app.post('/', function (req, res) {
   let city = req.body.city;
   let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-  console.log(url)
 
   request(url, function (err, response, body) {
-    if(err){
-      console.log('test')
-      res.render('index', {weather: null, error: 'Error, please try again'});
+    if (err) {
+      res.render('index', { weather: null, error: 'Error, please try again' });
     } else {
       let weather = JSON.parse(body)
-      if(weather.main == undefined){
-        console.log('test2')
-
-        res.render('index', {weather: null, error: 'Error, please try again'});
+      if (weather.main == undefined) {
+        res.render('index', { weather: null, error: 'Error, please try again' });
       } else {
-        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-        res.render('index', {weather: weatherText, error: null});
+        let weatherText = `It's ${weather.main.temp} F in ${weather.name}!`;
+        res.render('index', { weather: weatherText, error: null });
       }
     }
   });
